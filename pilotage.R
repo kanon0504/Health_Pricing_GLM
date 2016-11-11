c70f0617ea6dedcf007cfd09c344f82357143172
Chao Jun lIU <chaojun.maths@gmail.com>
2016-11-11 12:50:56 +0100

    second commit test for adding some comments

--- pilotage.R
+++ pilotage.R
@@ -1,6 +1,16 @@
 library(sas7bdat)
 
+# Load datasets
 panel_ass <- read.sas7bdat("/Users/Kanon/Downloads/panel_ass.sas7bdat")
+panel_generaliste <- read.sas7bdat("/Users/Kanon/Downloads/panel_generaliste_decompressed.sas7bdat")
+
+# Check the format of each data set
+is.data.frame(panel_ass)
+is.data.frame(panel_generaliste)
+
+# Convert all data.frame objects into data.table objects
+panel_ass <- as.data.frame.table(panel_ass)
+panel_generaliste <- as.data.frame.table(panel_generaliste)
 
 # A function that allows to show the schema of a dataframe with all variables converted to factors
 show_data_as_factor <- function(x)
@@ -16,4 +26,4 @@ sort(table(head(panel_ass[panel_ass$sexe == "I",],918)$type_assure))
 
 # Check the consistancy of the relationship of multiple variables(logical check)
 logical_check <- function()
-
+  

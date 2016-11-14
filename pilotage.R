5fcef76937377d7bf2fe82c6501e4852e096c2bf
kanon0504 <chaojun.maths@gmail.com>
2016-11-14 14:28:32 +0100

    Update pilotage.R
    Update on dataset panel_generaliste (standarlisation) 14 Nov

--- pilotage.R
+++ pilotage.R
@@ -1,5 +1,23 @@
 library(sas7bdat)
 
+###################### Functions and tests ###################### 
+
+# A function that allows to show the schema of a dataframe with all variables converted to factors
+show_data_as_factor <- function(x)
+{
+  if(length(levels(as.factor(x))) <= 50)
+  {return(levels(as.factor(x)))}
+  else
+  {return(length(levels(as.factor(x))))}
+}
+
+# See the discrete conditional distribution given some condition
+sort(table(head(panel_ass[panel_ass$sexe == "I",],918)$type_assure))
+
+###################### Functions and tests ###################### 
+
+
+
 # Load datasets on Windows
 panel_ass <- read.sas7bdat("C:/Users/s636000/Documents/Expat/data/MSH/panel_ass.sas7bdat")
 panel_generaliste <- read.sas7bdat("C:/Users/s636000/Documents/Expat/data/MSH/panel_generaliste_decompressed.sas7bdat")
@@ -21,17 +39,15 @@ is.data.frame(panel_generaliste)
 panel_ass <- as.data.frame.table(panel_ass)
 panel_generaliste <- as.data.frame.table(panel_generaliste)
 
-# A function that allows to show the schema of a dataframe with all variables converted to factors
-show_data_as_factor <- function(x)
-{
-  if(length(levels(as.factor(x))) <= 50)
-  {return(levels(as.factor(x)))}
-  else
-  {return(length(levels(as.factor(x))))}
-}
+# Set the pk (primary key) equals to paste(annee,ident_personne) in preparation for left join
+panel_ass$pk <- as.numeric(paste(panel_ass$annee,panel_ass$ident_personne,sep=""))
 
-# See the discrete conditional distribution given some condition
-sort(table(head(panel_ass[panel_ass$sexe == "I",],918)$type_assure))
+# Preparation with dataset panel_generaliste for consistancy
+panel_generaliste$Var1 <- NULL
+panel_generaliste$Var2 <- NULL
+names(panel_generaliste) <- c("ident_personne", "presence", "annee_soin", "somme_frais", "somme_quantite")
+panel_generaliste$fk <- as.numeric(paste(panel_generaliste$annee,panel_generaliste$ident_personne,sep=""))
 
-# Check the consistancy of the relationship of multiple variables(logical check)
-logical_check <- function()
+
+
+

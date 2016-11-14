40c0d07fd869e29e871a2a658cd9318af659cd0f
Chao Jun lIU <chaojun.maths@gmail.com>
2016-11-14 17:01:08 +0100

    preprocessing only with panel_ass and panel_generaliste

--- pilotage.R
+++ pilotage.R
@@ -11,6 +11,16 @@ show_data_as_factor <- function(x)
   {return(length(levels(as.factor(x))))}
 }
 
+# A function which returns the names of columns that contains NA value and the number of NA values it contains
+detection_NA <- function(DATASET)
+{
+  f<- function(x)
+  {return(length(which(is.na(x))))}
+  raw <- sapply(DATASET, f)
+  return(raw[which(raw != 0)])
+}
+
+
 # See the discrete conditional distribution given some condition
 sort(table(head(panel_ass[panel_ass$sexe == "I",],918)$type_assure))
 
@@ -24,7 +34,6 @@ sort(table(head(panel_ass[panel_ass$sexe == "I",],918)$type_assure))
 # panel_hospi <- read.sas7bdat("C:/Users/s636000/Documents/Expat/data/MSH/panel_hospi_decompressed.sas7bdat")
 # panel_bilan <- read.sas7bdat("C:/Users/s636000/Documents/Expat/data/MSH/panel_bilan_decompressed.sas7bdat")
 
-
 # Load datasets on Macbook Pro
 panel_ass <- read.sas7bdat("/Users/Kanon/Google Drive/AXA/data/MSH/panel_ass.sas7bdat")
 panel_generaliste <- read.sas7bdat("/Users/Kanon/Google Drive/AXA/data/MSH/panel_generaliste_decompressed.sas7bdat")
@@ -34,20 +43,30 @@ panel_bilan <- read.sas7bdat("/Users/Kanon/Google Drive/AXA/data/MSH/panel_bilan
 # Check the format of each data set
 is.data.frame(panel_ass)
 is.data.frame(panel_generaliste)
+is.data.frame(panel_hospi)
+is.data.frame(panel_bilan)
 
 # Convert all data.frame objects into data.table objects
 panel_ass <- as.data.frame.table(panel_ass)
 panel_generaliste <- as.data.frame.table(panel_generaliste)
+panel_hospi <- as.data.frame.table(panel_hospi)
+panel_bilan <- as.data.frame.table(panel_bilan)
 
 # Set the pk (primary key) equals to paste(annee,ident_personne) in preparation for left join
 panel_ass$pk <- as.numeric(paste(panel_ass$annee,panel_ass$ident_personne,sep=""))
 
 # Preparation with dataset panel_generaliste for consistancy
-panel_generaliste$Var1 <- NULL
-panel_generaliste$Var2 <- NULL
-names(panel_generaliste) <- c("ident_personne", "presence", "annee_soin", "somme_frais", "somme_quantite")
-panel_generaliste$fk <- as.numeric(paste(panel_generaliste$annee,panel_generaliste$ident_personne,sep=""))
+panel_generaliste$pk <- as.numeric(paste(panel_generaliste$annee,panel_generaliste$ident_personne,sep=""))
+dedup_panel_generaliste <- unique(panel_generaliste)
+rm(panel_generaliste)
+dedup_panel_generaliste$ident_personne <- NULL
+dedup_panel_generaliste$annee_soin <- NULL
+dedup_panel_generaliste$somme_frais <- NULL
+dedup_panel_generaliste$presence <- NULL
 
-
-
+# Left outer join of panel_ass and dedup_panel_generaliste
+data_generaliste <- merge(panel_ass,dedup_panel_generaliste, by = "pk", all.x = TRUE)
+data_generaliste$somme_quantite[is.na(data_generaliste$somme_quantite)] <- 0
+head(data_generaliste)
+detection_NA(data_generaliste)
 

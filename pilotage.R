89256a6bb86c3df00b9017eb35dca4456d112fe2
Chao Jun lIU <chaojun.maths@gmail.com>
2016-11-16 11:46:17 +0100

    add kpi_gini function

--- pilotage.R
+++ pilotage.R
@@ -133,6 +133,341 @@ eliminate_negative <- function(DATASET)
   DATASET <- DATASET[-to_eliminate,]
   return(DATASET)
 }
+
+## EXTERNALS ###########################################################
+########################################################################
+# Gain Curve
+# plots the gain (lorenz) curve for the given data and returns the gini index
+plotgain <- function(predrisk, truerisk = NULL, exposure = NULL, nbpts = 1000,
+                     significance = 2, normalize = F, return_val = F,
+                     with_optimal = F)
+{
+  ## check argument value
+  predrisk      = check_predrisk_gain(predrisk)
+  truerisk      = check_truerisk_gain(predrisk, truerisk)
+  exposure      = check_exposure_gain(predrisk, exposure)
+  significance  = check_significance_gain(significance)
+  nm_mod        = names(predrisk)
+  
+  ##plot
+  #colors
+  clrs = colorRampPalette(c("#3f88c5","#00b34d", "#ff3300","#ffd500","#7d00b3"))(length(predrisk))
+  
+  #other options
+  op <- list()
+  op$chart <- list(zoomType = "xy")
+  op$title <- list(text = 'Gain Curve', x = -20)
+  op$tooltip = F
+  op$xAxis <- list(list(min           = 0,
+                        max           = 1,
+                        title         = list(text = "population"),
+                        gridLineWidth = 0.5,
+                        gridLineDashStyle =  'longdash'))
+  op$yAxis <- list(list(min           = 0,
+                        max           = 1,
+                        title         = list(text = "risk"),
+                        gridLineWidth = 0.5,
+                        gridLineDashStyle =  'longdash'))
+  op$series <- list(list(name = "bisector",
+                         data   = list(c(0,0), c(1,1)),
+                         marker = list(enabled = F),
+                         showInLegend =  F,
+                         lineWidth = 1,
+                         color = "grey"))
+  
+  tit_legend = ifelse(normalize, "gini index (normalized)", "gini index")
+  op$legend <- list(title = list(text = tit_legend,
+                                 style = list("font-size"= "120%",
+                                              "text-align" = "center")),
+                    layout = 'vertical',
+                    align = 'right',
+                    verticalAlign = 'middle',
+                    backgroundColor = '#FCFFC5',
+                    borderColor = '#C98657',
+                    itemStyle = list(fontWeight = "normal"))
+  
+  
+  perfect_list = list()
+  for(i in 1:length(predrisk))
+  {
+    gaincd   = get_gaincoord(predrisk[[i]], truerisk[[i]],
+                             exposure[[i]], nbpts)
+    perfect_gaincd = get_gaincoord(truerisk[[i]], truerisk[[i]],
+                                   exposure[[i]], nbpts)
+    
+    perfect_list[[i]] = perfect_gaincd
+    
+    ## gini index
+    add_gini = calc_gini(gaincd)
+    if(normalize)
+      add_gini = add_gini/calc_gini(perfect_gaincd)
+    add_gini = signif(add_gini, significance)
+    if(i == 1)
+      gini = add_gini
+    else
+      gini = c(gini, add_gini)
+    
+    if(length(grep("^#[0-9]*$", nm_mod[i]) >=1))
+      nm_serie = paste0(i, " (", add_gini,")")
+    else
+      nm_serie = paste0(i, ": ", nm_mod[i], " (", add_gini,")")
+    add_series <- list(list(name   = nm_serie,
+                            data   = hc_listpts(gaincd[["x"]], gaincd[["y"]]),
+                            marker = list(enabled = F),
+                            color  = clrs[i]))
+    op$series = append(op$series, add_series)
+  }
+  ## perfect list cleanup
+  if(with_optimal)
+  {
+    names(perfect_list) = nm_mod
+    ret_list            = group_identicals(perfect_list)
+    perfect_list        = ret_list[['perfect_list']]
+    colors_ID           = ret_list[['colors_ID']]
+    
+    for(k in 1:length(perfect_list))
+    {
+      nms = names(perfect_list)
+      coord = perfect_list[[k]]
+      colID = colors_ID[k]
+      
+      perfect_gini = signif(calc_gini(coord), significance)
+      nm_serie = paste0("optimal_",nms[k], " (", perfect_gini,")")
+      add_series <- list(list(name   = nm_serie,
+                              data   = hc_listpts(coord[["x"]], coord[["y"]]),
+                              marker = list(enabled = F),
+                              color = clrs[colID],
+                              dashStyle = "ShortDot"))
+      op$series = append(op$series, add_series)
+    }
+  }
+  
+  ## plot
+  plt = highcharter::highchart(op)
+  print(plt)
+  
+  if(return_val)
+  {
+    ret = list(plot = plt, gini = gini)
+    return(ret)
+  }
+}
+
+
+#' Gini Index
+#'
+#' Calculate the AUC for a binary classifier
+#'
+#' @param predrisk predicted risk
+#' @param truerisk true risk
+#' @param exposure exposure
+#' @param significance number of decimal places for the auc
+#' @param normalize [bool] normalize the gini index
+#'
+#' @return gini indexes for the given (predrisk, truerisk, exposure)
+#'
+#' @export
+kpi_gini <- function(predrisk, truerisk = NULL, exposure = NULL, significance = 2,
+                     normalize = F, nbpts = 1000)
+{
+  predrisk      = check_predrisk_gain(predrisk)
+  truerisk      = check_truerisk_gain(predrisk, truerisk)
+  exposure      = check_exposure_gain(predrisk, exposure)
+  significance  = check_significance_gain(significance)
+  nm_mod        = names(predrisk)
+  
+  gini <- sapply(1:length(predrisk), function(i){
+    gaincd = get_gaincoord(predrisk[[i]], truerisk[[i]],
+                           exposure[[i]], nbpts)
+    return(calc_gini(gaincd))
+  })
+  
+  if(normalize)
+  {
+    giniPerfect <- sapply(1:length(predrisk), function(i){
+      gaincd = get_gaincoord(truerisk[[i]], truerisk[[i]], exposure[[i]],
+                             nbpts)
+      return(calc_gini(gaincd))
+    })
+    gini = gini / giniPerfect
+  }
+  
+  gini = signif(gini, significance)
+  names(gini) = nm_mod
+  
+  return(gini)
+}
+
+
+### INTERNALS ##########################################################
+########################################################################
+
+## non-vectorized function for calculating the (x,y) coordinates of a
+## gain curve given thre predicted risk, true risk and exposure
+get_gaincoord <- function(predrisk_vec, truerisk_vec, exposure_vec,
+                          nbpts = 1000)
+{
+  ordpred = order(predrisk_vec, decreasing = T)
+  cumrisk = c(0, cumsum(truerisk_vec[ordpred])/sum(truerisk_vec))
+  
+  if(is.null(exposure_vec))
+  {
+    x        = (1:nbpts)/nbpts
+    y        = quantile(cumrisk, x)
+    names(y) = NULL
+  }
+  else
+  {
+    cumexpo = c(0, cumsum(exposure_vec[ordpred])/sum(exposure_vec))
+    ind     = sample(x = 1:length(cumrisk), size = nbpts)
+    ind     = ind[order(ind)]
+    x       = cumexpo[ind]
+    y       = cumrisk[ind]
+  }
+  return(list(x = x, y = y))
+}
+
+
+## non-vectorized function for calculating the gini index given
+## (x,y) coordinates of the gain curve
+calc_gini <- function(x, y = NULL, significance = NULL)
+{
+  if(is.null(y))
+  {
+    temp = x
+    x = temp[[1]]
+    y = temp[[2]]
+  }
+  gini = (trapz(x, y)-0.5)/0.5
+  if(is.null(significance))
+    return(gini)
+  else
+    return(signif(gini, significance))
+}
+
+### CHECKARGS ##########################################################
+########################################################################
+
+check_significance_gain <- function(significance)
+{
+  if(!is.numeric(significance))
+    stop("significance should be a numeric")
+  if(length(significance) != 1)
+    stop("significance should be of length 1")
+  if(as.integer(significance) != significance)
+    warning("converting significance to integer")
+  significance = as.integer(significance)
+  if(!(significance >=0))
+    stop("significance should be a positive integer")
+  
+  return(significance)
+}
+
+check_predrisk_gain <- function(predrisk)
+{
+  ## check predrisk
+  if(class(predrisk)[1] != "list" & !is.numeric(predrisk))
+    stop("predrisk should be of type list or numeric")
+  if(is.numeric(predrisk))
+    predrisk = list(predrisk)
+  for(i in 1:length(predrisk))
+  {
+    if(!is.numeric(predrisk[[i]]))
+      stop(paste0("predrisk[[", i, "]] is not numeric"))
+    if(length(predrisk[[i]]) == 0)
+      stop(paste0("predrisk[[", i, "]] is of length 0"))
+  }
+  
+  ## check model name
+  if(is.null(names(predrisk)))
+    names(predrisk) = paste0("#", 1:length(predrisk))
+  else if(length(grep("^$", names(predrisk))) > 0)
+    for(i in grep("^$", names(predrisk)))
+      names(predrisk)[i] = paste0("#",i)
+    
+    return(predrisk)
+}
+
+check_truerisk_gain <- function(predrisk, truerisk)
+{
+  ## check truerisk
+  if(is.null(truerisk))
+  {
+    truerisk = predrisk
+    return(truerisk)
+  }
+  if(class(truerisk)[1] != "list" & !is.numeric(truerisk))
+    stop("truerisk should be of type list or numeric")
+  if(is.numeric(truerisk))
+    truerisk = list(truerisk)
+  if(length(truerisk) > length(predrisk))
+    stop("length(truerisk) > length(predrisk)")
+  else if(length(truerisk) < length(predrisk))
+  {
+    if(length(truerisk) == 1)
+      for(i in 2:length(predrisk))
+        truerisk[[i]] = truerisk[[1]]
+      else
+        stop("length of truerisk > 1 but doesnt match the length of predrisk")
+  }
+  
+  for(i in 1:length(truerisk))
+  {
+    if(!is.numeric(truerisk[[i]]))
+      stop(paste0("truerisk[[", i, "]] is not numeric"))
+    if(length(truerisk[[i]]) != length(predrisk[[i]]))
+      stop(paste0("length(predrisk[[", i, "]]) != length(truerisk[[",i, "]])"))
+  }
+  
+  ## check model name
+  if(!is.null(names(truerisk)))
+  {
+    warning("names are not considered for argument 'truerisk'. Matching with
+            'predrisk' is done only by order.")
+  }
+  
+  return(truerisk)
+  }
+
+check_exposure_gain <- function(predrisk, exposure)
+{
+  ## check exposure
+  if(is.null(exposure))
+    return(exposure)
+  if(class(exposure)[1] != "list" & class(exposure)[1] != "numeric")
+    stop("exposure should be of type list or numeric")
+  if(is.numeric(exposure))
+    exposure = list(exposure)
+  if(length(exposure) > length(predrisk))
+    stop("length(exposure) > length(predrisk)")
+  else if(length(exposure) < length(predrisk))
+  {
+    if(length(exposure) == 1)
+      for(i in 2:length(predrisk))
+        exposure[[i]] = exposure[[1]]
+      else
+        stop("length of exposure > 1 but doesnt match the length of predrisk")
+  }
+  for(i in 1:length(exposure))
+  {
+    if(!is.numeric(exposure[[i]]))
+      stop(paste0("exposure[[", i, "]] is not numeric"))
+    if(length(exposure[[i]]) != length(predrisk[[i]]))
+      stop(paste0("length(predrisk[[", i, "]]) != length(exposure[[",i, "]])"))
+    if(!all(exposure[[i]] >= 0 & exposure[[i]] <= 1))
+      stop(paste0("some elements of exposure[[", i, "]] are not in [0,1]"))
+  }
+  
+  ## check model name
+  if(!is.null(names(exposure)))
+  {
+    warning("names are not considered for argument 'exposure'. Matching with
+            'predrisk' is done only by order.")
+  }
+  
+  return(exposure)
+  }
+
 ############################ Functions and tests ############################ 
 
 ############################ Data Pre-processing ############################ 
@@ -231,7 +566,8 @@ glm_model <- glm(fmla, family = poisson(link = "log"), offset = tr$presence, dat
 
 ############################ Testing of glm_model model ############################ 
 
+prediction <- predict(glm_model, te[,xnames], type = 'response')
 
-
+kpi <- kpi_gini(predrisk = prediction, truerisk = te$somme_quantite, exposure = te$presence, significance = 6)
 ############################ Testing of glm_model model ############################ 
 

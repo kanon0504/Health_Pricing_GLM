eca94efe022c7d4b29177236ca5958d63c322406
Chao Jun lIU <chaojun.maths@gmail.com>
2016-12-16 10:46:32 +0100

    add merge_data function and check_distribution function

--- pilotage.R
+++ pilotage.R
@@ -78,6 +78,13 @@ kpi <- kpi_gini(predrisk = prediction, truerisk = te$somme_quantite
 print(kpi)
 ############################ Testing of glm_model model ############################ 
 
+
+
+
+
+
+
+
 merged_data[sexe != 'I',][type_assure != "E", .(freq_mean= mean(somme_quantite), age_m = mean(age)), by = c("annee","sexe")]
 
 mean_var <- data.frame(poste = c(1), mean = c(1), var = c(1), logmean = c(1), logvar = c(1))
@@ -94,4 +101,99 @@ for (name_claim in database)
 }
 
 
+setwd("/Users/Kanon/Google Drive/AXA/data/Merged_data")
+database <- list.files(path = '/Users/Kanon/Google Drive/AXA/data/Merged_data')
+for (name_claim_data in database)
+{
+  name <- strsplit(name_claim_data, split = ".", fixed = T)[[1]][1]
+  print(paste0("reading ", name))
+  data <- read.csv(paste0("/Users/Kanon/Google Drive/AXA/data/Merged_data/"
+                                         , name_claim_data))
+  assign(name, data)
+}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+merge_data(Xautres_protheses, "Xautres_protheses")
+merge_data(Xauxiliaires_medicaux,"Xauxiliaires_medicaux")
+merge_data(Xbilan_de_sante,"Xbilan_de_sante")
+merge_data(Xchambre_particuliere,"Xchambre_particuliere")
+merge_data(Xconsultation,"Xconsultation")
+merge_data(Xcures_thermales,"Xcures_thermales")
+merge_data(Xdivers,"Xdivers")
+merge_data(Xfiv,"Xfiv")
+merge_data(Xhospitalisation_sauf_chambre_particuliere,"Xhospitalisation_sauf_chambre_particuliere")
+merge_data(Ximplants_dentaires,"Ximplants_dentaires")
+merge_data(Xkeratotomie,"Xkeratotomie")
+merge_data(Xlentilles,"Xlentilles")
+merge_data(Xmaternite_sauf_fiv,"Xmaternite_sauf_fiv")
+merge_data(Xmedecine_alternative,"Xmedecine_alternative")
+merge_data(Xmontures,"Xmontures")
+merge_data(Xorthodontie,"Xorthodontie")
+merge_data(Xparadontologie,"Xparadontologie")
+merge_data(Xpetit_risque,"Xpetit_risque")
+merge_data(Xpharmacie,"Xpharmacie")
+merge_data(Xprothese_dentaire,"Xprothese_dentaire")
+merge_data(Xpsychiatrie,"Xpsychiatrie")
+merge_data(Xsoins_dentaires,"Xsoins_dentaires")
+merge_data(Xvaccination,"Xvaccination")
+merge_data(Xverres,"Xverres")
+
+
+
+Xautres_protheses <- c("autres_prothese","prothese_audio")
+Xauxiliaires_medicaux <- c("auxi_medical","kine")
+Xbilan_de_sante <- c("bilan","med_prevent")
+Xchambre_particuliere <- "chambre"
+Xconsultation <- c("specialiste","generaliste")
+Xcures_thermales <- "cures"
+Xdivers <- "divers"
+Xfiv <- "fertilite"
+Xhospitalisation_sauf_chambre_particuliere <- c("hospi","hospi_jour","chimio","lit_accompagnant","transport")
+Ximplants_dentaires <- "implants"
+Xkeratotomie <- "keratomie"
+Xlentilles <- "lentilles"
+Xmaternite_sauf_fiv <- c("cesarienne","maternite")
+Xmedecine_alternative <- c("acuponcture", "chiropractie","med_alter","osteopathie")
+Xmontures <- c("monture", "optique")
+Xorthodontie <- "orthodontie"
+Xparadontologie <- "paradontologie"
+Xpetit_risque <- c("petit_risque", "radiotherapie")
+Xpharmacie <- "pharmacie"
+Xprothese_dentaire <- "prothese_dentaire"
+Xpsychiatrie <- "psychiatrie"
+Xsoins_dentaires <- "dentaire_gen"
+Xvaccination <- "vaccins"
+Xverres <- "verres"
